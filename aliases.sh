d6ee7e754785fa07988bbe190df455b88ebdf5f7
Karl Fredrik <kf@caffe1.net>
2013-12-20 22:38:47 +0100

    Adding even more aliases

--- aliases.sh
+++ aliases.sh
@@ -4,3 +4,6 @@
 alias icanhas="sudo yum install -y"
 alias icanfind="yum search"
 alias update-all="yum check-update && sudo yum upgrade"
+
+# Various good-to-have stuff #
+alias ddg="lynx --accept_all_cookies duckduckgo.com/?q='$0'"
